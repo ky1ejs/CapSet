b50cd2632c6a9517f36d6e0e2f5bfce5d7047491
Kyle Satti <k@kylejs.dev>
2023-07-31 21:10:55 -0400

    Fix dSYM upload - auth token and error check

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,4 +6,7 @@
 #  Created by Kyle Satti on 7/31/23.
 #  
 
-brew install getsentry/tools/sentry-cli
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$BRANCH" == "main" ]]; then
+  brew install getsentry/tools/sentry-cli
+fi
--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -6,9 +6,17 @@
 #  Created by Kyle Satti on 7/31/23.
 #  
 
+export SENTRY_ORG=kylejs
+export SENTRY_PROJECT=setcap
+
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$BRANCH" != "main" ]]; then
+  echo "Skipping - dSYM upload to Sentry is only done on main"
+  exit 0
+fi
 
 if which sentry-cli >/dev/null; then
-    ERROR=$(sentry-cli upload-dif --include-sources "$CI_ARCHIVE_PATH/dSYMs" 2>$1 >/dev/null)
+    ERROR=$(sentry-cli upload-dif --include-sources "$CI_ARCHIVE_PATH/dSYMs" 2>&1 >/dev/null)
     if [ ! $? -eq 0 ]; then
         echo "warning: sentry-cli - $ERROR"
     fi
