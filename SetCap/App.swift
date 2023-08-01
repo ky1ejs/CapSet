e84553af810ad6bee958431ce65eddf56955d942
Kyle Satti <k@kylejs.dev>
2023-07-31 21:11:07 -0400

    Only boot sentry when on RELEASE config

--- SetCap/App.swift
+++ SetCap/App.swift
@@ -13,6 +13,7 @@ struct SetCapApp: App {
     @ObservedObject private var service = PhotoLibraryService()
 
     init() {
+        #if RELEASE
         SentrySDK.start { options in
             options.dsn = "https://637ab924a8eb9a421ea2886827f29769@o4504922846789632.ingest.sentry.io/4505625970147328"
             options.debug = true // Enabled debug when first installing is always helpful
@@ -21,6 +22,7 @@ struct SetCapApp: App {
             // We recommend adjusting this value in production.
             options.tracesSampleRate = 1.0
         }
+        #endif
     }
 
     var body: some Scene {
