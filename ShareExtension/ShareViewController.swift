ac5ed8b18c857015bc217d24b583512e3492ed95
Kyle Satti <k@kylejs.dev>
2023-08-13 15:33:14 -0400

    Add sentry to share extension

--- ShareExtension/ShareViewController.swift
+++ ShareExtension/ShareViewController.swift
@@ -10,9 +10,26 @@ import Social
 import SwiftUI
 import UniformTypeIdentifiers
 import SetCapUIKit
+import Sentry
 
 @objc(ShareViewController)
 class ShareViewController: UIViewController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        #if !DEBUG
+        SentrySDK.start { options in
+            options.dsn = "https://637ab924a8eb9a421ea2886827f29769@o4504922846789632.ingest.sentry.io/4505625970147328"
+
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+        }
+        #endif
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         let container = Container(doneAction: { [weak self] in
